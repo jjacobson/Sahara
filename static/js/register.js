ae214e5564becb8308e444ba79dd4c2a7ea09405
Jeremiah Jacobson <j.jacobson966@gmail.com>
2018-12-08 19:39:39 -0900

    minor style changes

--- static/js/register.js
+++ static/js/register.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     let password1 = document.getElementsByName("password1");
     let password2 = document.getElementsByName("password2");
-    password1[0].classList.add('form-control')
-    password2[0].classList.add('form-control')
+    password1[0].classList.add('form-control');
+    password2[0].classList.add('form-control');
+    password2[0].placeholder = 'Confirm Password';
 });
